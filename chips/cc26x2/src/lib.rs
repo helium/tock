d59273333274669b3b4bee2a3b11ccc9e3734365
Louis Thiery <louis@helium.com>
2018-09-30 09:58:57 -0700

    wip

--- chips/cc26x2/src/lib.rs
+++ chips/cc26x2/src/lib.rs
@@ -3,13 +3,18 @@
 #![crate_name = "cc26x2"]
 #![crate_type = "rlib"]
 extern crate cortexm4;
+extern crate cortexm;
 #[allow(unused_imports)]
 #[macro_use]
 extern crate kernel;
+#[macro_use]
+extern crate enum_primitive;
+extern crate num_traits;
 
 pub mod aon;
 pub mod chip;
 pub mod crt1;
+pub mod events;
 pub mod gpio;
 pub mod i2c;
 pub mod peripheral_interrupts;
